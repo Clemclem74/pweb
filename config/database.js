5aa954d419484b58ef6c2427844a0b40e4de02de
Clemclem74 <clementemil@aol.fr>
2019-05-13 17:54:39 +0200

    Problem database

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database : process.env.MONGODB_URI || 'mongodb://localhost:27017/pweb',
+    database : process.env.MONGODB_URI || 'mongodb://localhost/users',
     secret : 'secret'
 }
