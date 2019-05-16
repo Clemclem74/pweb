e272f00f418f407b1dacebca3b4d4dbf5ba52238
Clemclem74 <clementemil@aol.fr>
2019-05-17 01:15:00 +0200

    bugs + edit movie types + débuts affichage vu sur film (pas finit)

--- config/handlebars.js
+++ config/handlebars.js
@@ -1,3 +1,6 @@
+var Seen = require('./../models/seen');
+var Handlebars = require('handlebars');
+
 //Convertion clock
 const { format }  = require('helper-timeago');
 
@@ -24,12 +27,23 @@ helpers.math = function(lvalue, operator, rvalue, options) {
 helpers.checked = function(film,idType) {
   if (film.typeFilm.indexOf(idType) != -1) {
     console.log("true")
-    return "ckecked";
+    return new Handlebars.SafeString('checked');
   }
   else {
     console.log("false")
     return false;
   }
 };
+
+helpers.seen = function(idFilm , idUser) {
+  Seen.find({idFilm : idFilm , idUser : idUser}).then(seen => {
+    if (seen.length) {
+      console.log("vu detecté");
+      return new Handlebars.SafeString('<h1>pute</h1>');
+     // return new Handlebars.SafeString('<h1><span id="already-seen" class="badge badge-pill badge-success">Déjà vu</span></h1>');
+    }
+    return ''
+  })
+}
 
 module.exports = helpers
